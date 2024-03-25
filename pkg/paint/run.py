2cf77e434fd1a26c1f1db3c81f6dc16c75690ec2
Vlad <137103197+vladislav-serdyuk@users.noreply.github.com>
2024-03-25 21:45:32 +0300

    Update run.py

--- pkg/paint/run.py
+++ pkg/paint/run.py
@@ -24,7 +24,7 @@ class App(WindowGUI):
             else:
                 self.last_point = (-1, -1)
 
-            self.button(img, 0, self.title_h, 30, 30, (0, 0, 255), '', self.clean,
+            self.button(img, 0, 0, 30, 30, (0, 0, 255), '', self.clean,
                         fingers_touch, landmark)
             h, w, c = img.shape
             for i in range(self.win_w):
