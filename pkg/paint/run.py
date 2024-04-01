10662eed3bdedbc11a1ab7db844a6a5631365898
Vlad <137103197+vladislav-serdyuk@users.noreply.github.com>
2024-04-01 12:16:35 +0300

    Update run.py

--- pkg/paint/run.py
+++ pkg/paint/run.py
@@ -7,8 +7,8 @@ from GUIlib import WindowGUI
 class App(WindowGUI):
     def __init__(self):
         super().__init__()
-        self.win_h = 400
-        self.win_w = 400
+        self.win_h = 300
+        self.win_w = 300
         self.name = 'Paint'
         self.art = np.zeros((self.win_w, self.win_h))
         self.last_point = (-1, -1)
@@ -17,10 +17,11 @@ class App(WindowGUI):
         super().__call__(img, fingers_up, fingers_touch, landmark, buffer)
         if not self.hide:
             if (fingers_touch[0] and self.x <= landmark[8][0] <= self.x + self.win_w
-                    and self.y + self.title_h <= landmark[8][1] <= self.y + self.win_h):
+                    and self.y - self.win_h <= landmark[8][1] <= self.y):
                 if self.last_point != (-1, -1):
-                    cv2.line(self.art, self.last_point, (landmark[8][0] - self.x, landmark[8][1] - self.y), (1,), 2)
-                self.last_point = (landmark[8][0] - self.x, landmark[8][1] - self.y)
+                    cv2.line(self.art, self.last_point, (landmark[8][0] - self.x, landmark[8][1] - self.y + self.win_h),
+                             (1,), 2)
+                self.last_point = (landmark[8][0] - self.x, landmark[8][1] - self.y + self.win_h)
             else:
                 self.last_point = (-1, -1)
 
@@ -29,7 +30,7 @@ class App(WindowGUI):
             for i in range(self.win_w):
                 for j in range(self.win_h):
                     if self.art[i][j] == 1:
-                        img[max(min(self.y + i, h - 1), 0)][max(min(self.x + j, w - 1), 0)] = (0, 0, 0)
+                        img[max(min(self.y - self.win_h + i, h - 1), 0)][max(min(self.x + j, w - 1), 0)] = (0, 0, 0)
 
     def clean(self):
         self.art = np.zeros((self.win_w, self.win_h))
