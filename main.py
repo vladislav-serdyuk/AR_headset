aafd08908464fcf940cc18914610ff6b23894b2b
Vlad <137103197+vladislav-serdyuk@users.noreply.github.com>
2024-04-12 12:17:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import typing
+import json
+import importlib
 
 import cv2
 import numpy as np
@@ -6,12 +7,12 @@ from cvzone.SelfiSegmentationModule import SelfiSegmentation
 from cvzone.HandTrackingModule import HandDetector
 from flask import Flask, render_template, Response
 
-import GUImgr
 
 segmentor = SelfiSegmentation()  # remove background
 hand_detector = HandDetector()
 app = Flask(__name__)  # server
 cap = cv2.VideoCapture(0)
+app_buffer = []
 
 
 def process_image(frame: np.ndarray) -> np.ndarray:
@@ -24,31 +25,25 @@ def process_image(frame: np.ndarray) -> np.ndarray:
         fingers_up = hand_detector.fingersUp(hand1)
 
         fingers_touch = []
-
         for tip_id in [8, 12, 16, 20]:
-            length, info, _ = hand_detector.findDistance(landmark[4][0:2], landmark[tip_id][0:2],
-                                                         (frame if debug else None), color=(255, 0, 255), scale=10)
+            length, info, _ = hand_detector.findDistance(landmark[4][0:2], landmark[tip_id][0:2])
             if length < 32:
                 fingers_touch.append(1)
             else:
                 fingers_touch.append(0)
 
-        GUImgr.draw_gui(frame, fingers_up, fingers_touch, landmark)
+        for gui in Apps:
+            gui(frame, fingers_up, fingers_touch, landmark, app_buffer)
 
         if hand_on_gui:
-            min_x = bbox[0] - 40
-            max_x = bbox[0] + bbox[2] + 40
-            min_y = bbox[1] - 40
-            max_y = bbox[1] + bbox[3] + 40
-            if min_x <= 0:
-                min_x = 0
-            if min_y <= 0:
-                min_y = 0
+            min_x = max(bbox[0] - 20, 0)
+            max_x = bbox[0] + bbox[2] + 20
+            min_y = max(bbox[1] - 20, 0)
+            max_y = bbox[1] + bbox[3] + 20
 
             crop_img = copy_frame[min_y:max_y, min_x:max_x]
             src = segmentor.removeBG(crop_img, (0, 0, 0), 0.3)
-            _, alpha = cv2.threshold(cv2.cvtColor(src, cv2.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY)
-            frame[min_y:max_y, min_x:max_x][alpha != 0] = src[alpha != 0]
+            frame[min_y:max_y, min_x:max_x][src != (0, 0, 0)] = src[src != (0, 0, 0)]
 
         if debug:
             cv2.putText(frame, str(fingers_up), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
@@ -57,7 +52,8 @@ def process_image(frame: np.ndarray) -> np.ndarray:
                 cx, cy, cz = lm
                 cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
     else:
-        GUImgr.draw_gui(frame, [0] * 5, [0] * 4, [(0, 0)] * 20)
+        for gui in Apps:
+            gui(frame, [0] * 5, [0] * 4, [(0, 0)] * 20, app_buffer)
 
     return frame
 
@@ -67,40 +63,31 @@ def index() -> str:
     return render_template('index.html')
 
 
-@app.route('/video_feed')
-def video_feed() -> Response:
-    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
 @app.route('/script.js')
 def script() -> str:
     return render_template('script.js')
 
 
+@app.route('/video_feed')
+def video_feed() -> Response:
+    return Response(get_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 def get_frame():
     """
-    Получает изображение с камеры, обрабатывает, отправляет клиенту
-    :return: Generator[bytes, Any, None]
-    """
+        Получает изображение с камеры, обрабатывает, отправляет клиенту
+        :return: Generator[bytes, Any, None]
+        """
     while True:
         ret, frame = cap.read()  # get frame from capture
-        # frame = cv2.rotate(frame, 1)
-        if flip_img:
-            frame = cv2.flip(frame, 1)
-
         frame = process_image(frame)
         h, w, c = frame.shape
-        border = np.zeros((h, int(w*0.3), c), dtype=np.uint8)
+        border = np.zeros((h, int(w * 0.3), c), dtype=np.uint8)
         frame = np.concatenate((border, frame, border), axis=1)
-
         frame = np.concatenate((frame, frame), axis=1)
         if show_window:
             cv2.imshow('video', frame)
-
-            if cv2.waitKey(1) == ord('q'):
-                cap.release()
-                cv2.destroyAllWindows()
-                quit()
+            cv2.waitKey(1)
 
         _, buffer = cv2.imencode('.jpg', frame)
         yield (b'--frame\r\n'
@@ -110,11 +97,11 @@ def get_frame():
 # setting
 debug = True
 hand_on_gui = True
-flip_img = False
 show_window = True
-# end
 
-GUImgr.init_gui()
+with open('pkglist.json') as file:
+    Apps = [importlib.import_module('pkg.' + pkg['dir'] + '.run').App()
+            for pkg in json.JSONDecoder().decode(file.read()).values()]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
